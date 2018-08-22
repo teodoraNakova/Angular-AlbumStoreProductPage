f1cce2dbca360de8fe6e3ed8ba54899e72eb2951
Teodora Nakova <nakovat@nakovat-a01.vmware.com>
2018-08-22 11:38:10 +0300

    completed module 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import {Observable} from 'rxjs/Observable';
-import {Album} from './album';
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productUrl).map(response => <Product[]>response.json());
   }
 
 }
